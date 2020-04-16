d2fda4f8d4ceac14057664133b638a17bab86675
nikhilnagaraju <nikhilnagaraju96@gmail.com>
2020-04-16 12:03:20 +0530

    fix: eslint format

--- app.js
+++ app.js
@@ -2,22 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const responseTime = require('response-time');
-const appRoutes = require('./routes')
+const appRoutes = require('./routes');
 
 const app = express();
-
 
 app.set('port', process.env.PORT || 8989);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('common'));
-app.use(responseTime())
-app.use(appRoutes)
+app.use(responseTime());
+app.use(appRoutes);
 
-try {
+try{
   app.listen(app.get('port'));
-  console.log(`server is listening on ${app.get('port')}`)
-} catch (err) {
+  console.log(`server is listening on ${app.get('port')}`);
+}catch(err){
   console.error('error in starting server');
   process.exit(2);
 }
--- routes.js
+++ routes.js
@@ -1,45 +1,45 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const trafficFinesEndpoint = "https://www.karnatakaone.gov.in/PoliceCollectionOfFine/FineDetails";
+const trafficFinesEndpoint = 'https://www.karnatakaone.gov.in/PoliceCollectionOfFine/FineDetails';
 const regPattern = RegExp(/^([a-z,A-Z]{2})([0-6][0-9]|70)[a-z,A-Z]{0,2}[0-9]{4}$/);
 
 router.get('/', (req, res) => {
   res.send({
-    "status": "ok",
-    "message": "Why say hello world!, when you can say HELLO UNIVERSE!"
+    status: 'ok',
+    message: 'Why say hello world!, when you can say HELLO UNIVERSE!'
   });
-})
+});
 
 router.get('/fines', (req, res) => {
-  var registration = req.query.registration;
+  const registration = req.query.registration;
 
-  if (!registration || !regPattern.test(registration)) {
+  if(!registration || !regPattern.test(registration)){
     return res.status(400).send({
-      "status": "error",
-      "message": "A valid 'registration' param in 'KAXX(AZ)XXXX' format is required."
+      status: 'error',
+      message: 'A valid \'registration\' param in \'KAXX(AZ)XXXX\' format is required.'
     });
   }
 
-  var options = {
+  const options = {
     params: {
-      'SearchBy': 'REGNO',
-      'ServiceCode': 'BPS',
-      'SearchValue': registration,
+      SearchBy: 'REGNO',
+      ServiceCode: 'BPS',
+      SearchValue: registration
     }
-  }
-  axios.post(trafficFinesEndpoint, null, options).then(extResponse => {
+  };
+  return axios.post(trafficFinesEndpoint, null, options).then(extResponse => {
     res.send(extResponse.data);
   }).catch(err => {
     console.error(err);
     res.status(400).send(err);
-  })
+  });
 });
 
 router.get('**', (req, res) => {
   res.status(400).send({
-    "status": "error",
-    "message": "use '/fines' endpoint with 'registration' query param in 'KAXX(AZ)XXXX' format to check fines."
+    status: 'error',
+    message: 'use \'/fines\' endpoint with \'registration\' query param in \'KAXX(AZ)XXXX\' format to check fines.'
   });
 });
 
